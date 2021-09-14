47794970c99ee4ddd6c5db8dc5b9f27173b709b0
Reginald Gaines <token@dexmatic.finance>
2021-09-13 18:31:23 -0700

    logo
    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,7 +51,7 @@ module.exports = {
         'cyan-blue': '#0993EC',
         'dark-pink': '#221825',
         'dark-blue': '#0F182A',
-        'dark-1000': '#0D0415',
+        'dark-1000': '#eb080a',
         'dark-900': '#161522',
         'dark-850': '#1d1e2c',
         'dark-800': '#202231',
